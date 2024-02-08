eacb68643a97109610f73ff505225a9641c83a14
ayan4m1 <andrew@bulletlogic.com>
2024-02-08 04:43:13 -0500

    fix dates

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function IndexPage() {
           <source src="/beagle-zoom.webm" type="video/webm" />
         </video>
         <h1>Leela the Beagle</h1>
-        <h2>2012 &mdash; 2024</h2>
+        <h2>December 2011 &ndash; February 2024</h2>
         <h3>She will always be in our hearts.</h3>
       </Container>
     </Layout>
