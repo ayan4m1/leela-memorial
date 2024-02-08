6ef192446a4fe95462af5d8ce26e8cb38f51a55d
ayan4m1 <andrew@bulletlogic.com>
2024-02-08 01:02:27 -0500

    remove unnecessary <Fragment>

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-import { Fragment } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import {
   faCube,
@@ -12,29 +11,27 @@ import NavLink from 'components/navLink';
 
 export default function Header({ siteTitle }) {
   return (
-    <Fragment>
-      <Navbar expand="lg" bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand as="a" href="/" className="py-2">
-            {siteTitle}
-          </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Nav>
-              <NavLink to="/story" icon={faFileText} label="Her Story" />
-              <NavLink to="/images" icon={faImagePortrait} label="Images" />
-              <NavLink to="/3d" icon={faCube} label="3D View" />
-              <NavLink
-                external
-                to="https://bfp.org/"
-                icon={faHeart}
-                label="Contribute"
-              />
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Fragment>
+    <Navbar expand="lg" bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand as="a" href="/" className="py-2">
+          {siteTitle}
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav>
+            <NavLink to="/story" icon={faFileText} label="Her Story" />
+            <NavLink to="/images" icon={faImagePortrait} label="Images" />
+            <NavLink to="/3d" icon={faCube} label="3D View" />
+            <NavLink
+              external
+              to="https://bfp.org/"
+              icon={faHeart}
+              label="Contribute"
+            />
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
