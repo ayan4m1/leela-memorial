2241b36c307920903937348980f0a2852f230d69
ayan4m1 <andrew@bulletlogic.com>
2024-02-08 04:37:38 -0500

    add video from cinematic photo for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import Layout from 'components/layout';
-import { Container, Image } from 'react-bootstrap';
-
-import logo from '../images/gatsby-icon.png';
+import { Container } from 'react-bootstrap';
 
 export default function IndexPage() {
   return (
     <Layout title="Home" description="Memorial for my beagle.">
       <Container className="text-center">
-        <Image src={logo} alt="Beagle" rounded />
+        <video autoPlay muted loop height={512} style={{ borderRadius: 8 }}>
+          <source src="/beagle-zoom.webm" type="video/webm" />
+        </video>
         <h1>Leela the Beagle</h1>
         <h2>2012 &mdash; 2024</h2>
         <h3>She will always be in our hearts.</h3>
